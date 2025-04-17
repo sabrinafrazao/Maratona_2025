cde207bae3142c6121dac4d05b5f34d0b3ab490c
sabrinafrazao <sabrinadasilvafrazao@gmail.com>
2025-04-17 18:54:37 -0400

    Adicionando comentarios explicativo do código

--- beecrowd/2780.py
+++ beecrowd/2780.py
@@ -1,14 +1,17 @@
-
 d = int(input())
 
+# Verifica em qual faixa de distância 'd' se encontra e atribui o valor correspondente a 'p' que seria a quantidade de pontos
 if d <= 800:
-
+    # Se 'd' é menor ou igual a 800, 'p' é 1
     p = 1
 
-elif d > 800 and d <=1400:
+elif d > 800 and d <= 1400:
+    # Se 'd' está entre 801 e 1400, 'p' é 2
     p = 2
 
-elif d> 1400 and d <= 2000:
+elif d > 1400 and d <= 2000:
+    # Se 'd' está entre 1401 e 2000, 'p' é 3
     p = 3
+
 
 print(p)
