ba3106e1baa6a2a7837024839314b120028b1155
sabrinafrazao <sabrinadasilvafrazao@gmail.com>
2025-04-17 17:28:02 -0400

    Adicionando comentarios explicativo do código

--- beecrowd/1020.py
+++ beecrowd/1020.py
@@ -1,13 +1,18 @@
 idade = int(input())
 
+# Calcula quantos anos completos existem nos dias fornecidos
 ano = idade // 365
 
+# Atualiza o valor da idade para o restante dos dias após remover os anos completos
 idade = idade % 365
 
+# Calcula quantos meses completos existem no restante dos dias
 mes = idade // 30
 
+# Atualiza o valor da idade para o restante dos dias após remover os meses completos
 idade = idade % 30
 
+#O que sobrou são os dias restantes (menos de um mês)
 dias = idade
 
 print(f"{ano} ano(s)")
