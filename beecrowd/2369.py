16eb0aa9b169bfed72675a94a1b4bd7e12912dd9
sabrinafrazao <sabrinadasilvafrazao@gmail.com>
2025-04-17 18:39:38 -0400

    fazendo outra forma e adicionaando comentario explicativo no código

--- beecrowd/2369.py
+++ beecrowd/2369.py
@@ -1,35 +1,24 @@
+c = int(input())
 
-n = int(input())
+# Inicializa a variável valor, mas, poderia Inicializar com 7
+valor = 0
 
-valor = 7
+# Verifica em qual faixa o valor de c se encontra e calcula o valor correspondente
+if c <= 10:
+    # Se consumo é menor ou igual a 10m³, o valor é 7
+    valor = 7
+    
+elif c <= 30:
+    # Se consumo está entre 11 e 30m³, adiciona 1 real para cada unidade acima de 10m³
+    valor = 7 + (c - 10) * 1
+    
+elif c <= 100:
+    # Se consumo está entre 31 e 100m³, adiciona 20 reais (para os primeiros 30m³) e 2 reais para cada unidade acima de 30m³
+    valor = 7 + 20 + (c - 30) * 2
+    
+else:
+    # Se consumo é maior que 100m³, adiciona 140 reais (para os primeiros 100m³) e 5 reais para cada unidade acima de 100m³
+    valor = 7 + 20 + 140 + (c - 100) * 5
 
-# 11 a 30
-
-if n > 10:
-    if n <= 30:
-
-        valor = valor + (n - 10) * 1
-
-    else:
-
-        valor = valor + (20*1)  
-
-
-# 31 a 100
-
-if n > 30:
-
-    if n <= 100:
-
-        valor = valor + (n - 30) * 2
-
-    else:
-        valor = valor + (70*2)
-
-# acima de 100
-
-if n > 100:
-
-    valor = valor + (n - 100) * 5
 
 print(valor)
